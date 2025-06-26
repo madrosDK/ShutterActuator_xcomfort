8ee9a23bc20b019ffdf37d25bdb947a9908c5260
madrosDK <danielkoglek@gmx.at>
2025-06-26 16:05:00 +0200

    Update module.php

--- ShutterActuator/module.php
+++ ShutterActuator/module.php
@@ -296,10 +296,10 @@ class xcomfortshutter extends IPSModule
          $posBottom = $this->ReadPropertyInteger('position_bottom');
 
          // Bereichsgrenzen laden
-         $middleMin = $this->ReadPropertyInteger('middle_min',10);
-         $middleMax = $this->ReadPropertyInteger('middle_max',64);
-         $bottomMin = $this->ReadPropertyInteger('bottom_min',65);
-         $bottomMax = $this->ReadPropertyInteger('bottom_max',90);
+         $middleMin = $this->ReadPropertyInteger('middle_min');
+         $middleMax = $this->ReadPropertyInteger('middle_max');
+         $bottomMin = $this->ReadPropertyInteger('bottom_min');
+         $bottomMax = $this->ReadPropertyInteger('bottom_max');
 
          if ($vid != 0) {
              $this->SendDebug(__FUNCTION__, 'Angeforderte Position: ' . $value . '%');
